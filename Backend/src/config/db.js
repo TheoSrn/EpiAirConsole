1fb2db549a50e82c6e4aeac8b7d618fe0a81fd2c
Théo <91675378+TheoSrn@users.noreply.github.com>
2026-02-06 13:39:07 +0100

    [FIX] Serv.js app.js and db.js import

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,45 +1,15 @@
-import http from "http";
-import { Server } from "socket.io";
-import app from "../app.js";
-import dotenv from "dotenv";
-import { connectDB } from "./db.js";
-import cors from "cors";
+import mongoose from "mongoose";
 
-dotenv.config();
-
-const PORT = process.env.PORT || 5000;
-
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://epiairconsole.onrender.com"
-  ],
-  credentials: true
-}));
-
-const start = async () => {
+export const connectDB = async () => {
   try {
     if (!process.env.MONGO_URI) {
       console.warn("MONGO_URI non défini — connexion MongoDB non effectuée");
-    } else {
-      await connectDB();
+      return;
     }
-
-    const server = http.createServer(app);
-
-    const io = new Server(server, {
-      cors: {
-        origin: "*"
-      }
-    });
-
-    server.listen(PORT, () =>
-      console.log(`Serveur running on port ${PORT}`)
-    );
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connecté !");
   } catch (err) {
-    console.error("Erreur au démarrage du serveur:", err.message);
+    console.error("Erreur MongoDB:", err.message);
     process.exit(1);
   }
 };
-
-start();
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,31 +1,25 @@
-// Fusion d'express avec socket.io
-
 import http from "http";
 import { Server } from "socket.io";
-import app from "./app.js";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import app from "./app.js";
+import { connectDB } from "./config/connectDB.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
-	try {
-		if (!process.env.MONGO_URI) {
-			console.warn("MONGO_URI non défini — connexion MongoDB non effectuée");
-		} else {
-			await connectDB();
-		}
+  try {
+    await connectDB();
 
-		const server = http.createServer(app);
-		const io = new Server(server, { cors: { origin: "*" } });
+    const server = http.createServer(app);
+    const io = new Server(server, { cors: { origin: "*" } });
 
-		server.listen(PORT, () => console.log(`Serveur running on port ${PORT}`));
-	} catch (err) {
-		console.error("Erreur au démarrage du serveur:", err.message);
-		process.exit(1);
-	}
+    server.listen(PORT, () => console.log(`Serveur running on port ${PORT}`));
+  } catch (err) {
+    console.error("Erreur au démarrage du serveur:", err.message);
+    process.exit(1);
+  }
 };
 
 start();
