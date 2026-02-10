706ffa90d71f30eeffeb18d8af3b32a7ece18b4b
Théo <91675378+TheoSrn@users.noreply.github.com>
2026-02-11 00:13:11 +0100

    [FIX] CORS Authorization for frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,8 +14,10 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://epiairconsole.onrender.com"
+    "https://epi-air-console.vercel.app"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
